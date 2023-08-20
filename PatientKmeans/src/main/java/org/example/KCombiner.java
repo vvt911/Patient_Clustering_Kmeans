e446077f7c97d99df1d7ffe74eca2798c81f6e83
ttt1804 <trantan1804@gmail.com>
2023-08-20 13:07:58 +0700

    finish

--- PatientKmeans/src/main/java/org/example/KCombiner.java
+++ PatientKmeans/src/main/java/org/example/KCombiner.java
@@ -9,15 +9,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class KCombiner extends Reducer<LongWritable, PointWritable, LongWritable, PointWritable> {
 
     public void reduce(LongWritable centroidId, Iterable<PointWritable> points, Context context)
             throws IOException, InterruptedException {
-
-        List<PointWritable> pointList = new ArrayList<>();
 
         String outputFolder = "/patient-output-points/";
         String outputFileName = "Cluster" + centroidId.toString() + ".txt";
@@ -26,22 +22,24 @@ public class KCombiner extends Reducer<LongWritable, PointWritable, LongWritable
         FSDataOutputStream dos = hdfs.create(new Path(outputFolder + outputFileName), true);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(dos));
 
-        bw.write("Cluster" + centroidId.toString() + ": ");
+        bw.write("Cluster" + centroidId + ": ");
         bw.newLine();
 
-        for (PointWritable point : points) {
-            bw.write(point.toString());
+        PointWritable ptSum = PointWritable.copy(points.iterator().next());
+        PointWritable firstPoint = PointWritable.copy(ptSum);
+
+        bw.write(firstPoint.toString());
+        bw.newLine();
+
+        while (points.iterator().hasNext()) {
+            PointWritable currentPoint = points.iterator().next();
+            bw.write(currentPoint.toString());
             bw.newLine();
-            pointList.add(PointWritable.copy(point));
+            ptSum.sum(currentPoint);
         }
 
         bw.close();
         hdfs.close();
-
-        PointWritable ptSum = PointWritable.copy(pointList.get(0));
-        for (int i = 1; i < pointList.size(); i++) {
-            ptSum.sum(pointList.get(i));
-        }
 
         context.write(centroidId, ptSum);
     }
